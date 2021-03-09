8380cdf528cbc3cbf9ba62ccb8de606423fdda1c
Dylan Smith <dylan-smith@github.com>
2021-03-09 14:36:16 -0600

    add chmod

--- run-action.ps1
+++ run-action.ps1
@@ -1,17 +1,23 @@
 if($IsWindows -eq $True)
 {
     $filePath = Join-Path $PSScriptRoot "/dist/win-x64/dotnet-xplat-action.exe"
+    chmod +x $filePath
+    Write-Output "Running $filePath..."
     & $filePath
 }
 
 if($IsLinux -eq $True)
 {
     $filePath = Join-Path $PSScriptRoot "/dist/linux-x64/dotnet-xplat-action"
+    chmod +x $filePath
+    Write-Output "Running $filePath..."
     & $filePath
 }
 
 if($IsMacOS -eq $True)
 {
     $filePath = Join-Path $PSScriptRoot "/dist/osx-x64/dotnet-xplat-action"
+    chmod +x $filePath
+    Write-Output "Running $filePath..."
     & $filePath
 }
